6e897f4b476487a5daf651e3bdcf6abac771fc45
fernando <chaoslecion71@gmail.com>
2022-10-01 15:47:13 -0500

    code host name

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['3.93.234.41'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['testsoverodjango.ml'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
